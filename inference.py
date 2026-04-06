77638baf205d5e6a707402a6be5129c1dbf30ad6
suyash-sketch <khandagalesuyash05@gmail.com>
2026-04-06 21:27:57 +0530

    update gpt-oss-120b model to use hf

--- inference.py
+++ inference.py
@@ -33,7 +33,7 @@ except ImportError:
 load_dotenv()
 
 API_BASE_URL = os.getenv("API_BASE_URL") or "https://router.huggingface.co/v1"
-MODEL_NAME = os.getenv("MODEL_NAME") or "Qwen/Qwen2.5-72B-Instruct"
+MODEL_NAME = os.getenv("MODEL_NAME") or "openai/gpt-oss-120b:groq"
 API_KEY = os.getenv("API_KEY") or os.getenv("HF_TOKEN")
 IMAGE_NAME = os.getenv("IMAGE_NAME") or os.getenv("LOCAL_IMAGE_NAME")
 
